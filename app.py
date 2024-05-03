a5100557dd4298d0d4eb6a110c346a39e8c5921e
merilmiranda <ratagmiranda@gmail.com>
2024-05-04 00:10:16 +0800

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 import os
 from os.path import join, dirname
-from dotenv import load_dotenv 
-from flask import (
-     Flask , 
-     request, 
-     render_template, 
-     redirect, 
-     url_for, 
-     jsonify
-)
+from dotenv import load_dotenv
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
@@ -21,14 +14,13 @@ MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-
 db = client[DB_NAME]
 
-app = Flask(__name__)
+app =  Flask(__name__)
 
 @app.route('/')
 def main():
-    words_result = db.meril.find({}, {'_id': False})
+    words_result = db.words.find({}, {'_id': False})
     words = []
     for word in words_result:
         definition = word['definitions'][0]['shortdef']
@@ -38,7 +30,11 @@ def main():
             'definition': definition,
         })
     msg = request.args.get('msg')
-    return render_template('index.html', words=words, msg=msg)
+    return render_template(
+        'index.html',
+        words=words,
+        msg=msg
+    )
 
 @app.route('/detail/<keyword>')
 def detail(keyword):
@@ -46,22 +42,20 @@ def detail(keyword):
     url = f'https://dictionaryapi.com/api/v3/references/collegiate/json/{keyword}?key={api_key}'
     response = requests.get(url)
     definitions = response.json()
-
     if not definitions:
-        return render_template('error.html', word=keyword)
+        return redirect(url_for('error', keyword=keyword))
 
     if type(definitions[0]) is str:
         suggestions = ', '.join(definitions)
-        return render_template('error.html', word=keyword, suggestions=suggestions)
+        return redirect(url_for('error', keyword=keyword, suggestions=suggestions))
 
-    status = request.args.get('status_give', 'new')
+    status=request.args.get('status_give', 'new')
     return render_template(
         'detail.html',
         word=keyword,
         definitions=definitions,
-        status=status,
+        status=status
     )
-
 
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
@@ -73,37 +67,38 @@ def save_word():
         'definitions': definitions,
         'date': datetime.now().strftime('%Y-%m-%d'),
     }
-    db.meril.insert_one(doc)
+    db.words.insert_one(doc)
     return jsonify({
         'result': 'success',
-        'msg': f'the word, {word}, was saved!!!'
+        'msg': f'the word, {word}, was saved'                
     })
-
 
 @app.route('/api/delete_word', methods=['POST'])
 def delete_word():
     word = request.form.get('word_give')
-    db.meril.delete_one({'word': word})
+    db.words.delete_one({'word': word})
+    db.examples.delete_many({'word': word})
     return jsonify({
         'result': 'success',
-        'msg': f'the word {word} was deleted'
+        'msg': f'the word, {word}, was deleted'                
     })
+    
+@app.route('/error/<keyword>')
+def error(keyword):
+    suggestions = request.args.get('suggestions')
+    return render_template('error.html', keyword=keyword, suggestions=suggestions)
 
 @app.route('/api/get_exs', methods=['GET'])
 def get_exs():
     word = request.args.get('word')
-    example_data = db.examples.find({'word':word})
+    example_data = db.examples.find({'word': word})
     examples = []
     for example in example_data:
         examples.append({
             'example': example.get('example'),
-            'id': str(example.get('_id')),
-
+            'id': str(example.get('_id'))
         })
-    return jsonify({
-        'result': 'success',
-        'examples': examples
-        })
+    return jsonify({'result': 'success', 'examples': examples})
 
 @app.route('/api/save_ex', methods=['POST'])
 def save_ex():
@@ -111,24 +106,18 @@ def save_ex():
     example = request.form.get('example')
     doc = {
         'word': word,
-        'example': example,
+        'example': example
     }
     db.examples.insert_one(doc)
-    return jsonify({
-        'result': 'success',
-        'msg' : f'Your example, {example}, for the word, {word}, was saved'
-    })
+    return jsonify({'result': 'success', 'msg': f'your example, {example}, for the word, {word}, was saved!'})
 
 
 @app.route('/api/delete_ex', methods=['POST'])
 def delete_ex():
     id = request.form.get('id')
     word = request.form.get('word')
-    db.examples.delete_one({'_id': ObjectId (id)})
-    return jsonify({
-        'result': 'success',
-        'msg' : f'Your  example for the word, {word}, was deleted'
-    })
+    db.examples.delete_one({'_id': ObjectId(id)})
+    return jsonify({'result': 'success', 'msg': f'your example for the word, {word}, was deleted!'})
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
